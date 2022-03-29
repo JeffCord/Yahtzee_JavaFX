7c4d7c4e03013f4101dec6a7aeacd616180d3fc8
JeffCord <jeffcord97@gmail.com>
2022-03-28 21:50:23 -0500

    Num of Players scene w/ basic code

--- YahtzeeApp/YahtzeeApp/src/application/controller/PlayerSelectController.java
+++ YahtzeeApp/YahtzeeApp/src/application/controller/PlayerSelectController.java
@@ -1,10 +1,10 @@
 package application.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import java.io.File;
-import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,50 +15,51 @@ import javafx.fxml.Initializable;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 
-public class MainController implements Initializable {
-
+public class PlayerSelectController implements Initializable {
+	
+	static final int MAX_NUM_OF_PLAYERS = 6;
+    
 	@FXML
     private AnchorPane mainAnchorPane;
+    
+    @FXML
+    private Button confirmBtn;
+    
+    @FXML
+    private Button backToMainBtn;
 
     @FXML
-    private Button loadGameBtn;
-
+    private ChoiceBox<Integer> playerNumChoiceBox;
+    
     @FXML
-    private Button newGameBtn;
-
-    @FXML
-    private Button quitBtn;
-
-    @FXML
-    void loadBtnPressed(ActionEvent event) {
-    	System.out.println("Load");
-    }
-
-    @FXML
-    void newGameBtnPressed(ActionEvent event) {
-    	System.out.println("New");
+    void backToMainBtnPressed(ActionEvent event) {
+    	System.out.println("Back to Main pressed");
     	try {
-	    	URL url = new File("src/PlayerSelect.fxml").toURI().toURL(); // get the crossword fxml file
+	    	URL url = new File("src/Main.fxml").toURI().toURL(); // get the crossword fxml file
 			mainAnchorPane = FXMLLoader.load(url); // load the new pane
 	    	Scene scene = new Scene(mainAnchorPane); // set the new scene
 	    	Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow(); // set the new stage
 	    	window.setScene(scene); // place new stage on new stage
 	    	window.show(); // display the new stage
     	} catch (IOException e) {
-    		System.out.println("ERROR: could not find PlayerSelect.fxml file");
+    		System.out.println("ERROR: could not find Main.fxml file");
     	}
-
     }
 
     @FXML
-    void quitBtnPressed(ActionEvent event) {
-    	System.out.println("Quit");
+    void confirmBtnPressed(ActionEvent event) {
+    	System.out.println("Confirmed number of players");
     }
-
+    
 	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
+	public void initialize(URL location, ResourceBundle resources) {
 		this.mainAnchorPane.setStyle("-fx-background-color: #FF0000");
+		for (int i = 0; i < MAX_NUM_OF_PLAYERS; i++) {
+			this.playerNumChoiceBox.getItems().add(i + 1);
+		}
+		
 	}
-
+	
 }
