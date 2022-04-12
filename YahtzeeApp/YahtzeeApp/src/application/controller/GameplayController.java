41ce97228eb0f99badc52db224319f1b9a48d952
JeffCord <jeffcord97@gmail.com>
2022-04-12 13:34:58 -0500

    minor changes to PlayerNames.fxml

--- YahtzeeApp/YahtzeeApp/src/application/controller/GameplayController.java
+++ YahtzeeApp/YahtzeeApp/src/application/controller/GameplayController.java
@@ -19,16 +19,5 @@ public class GameplayController implements Initializable {
 		
 	}
 	
-//	public GameplayController() {
-//		// create match and players
-//		match = new Match(numOfPlayers);
-//		for (int i = 0; i < numOfPlayers; i++) {
-//			String playerName = "Player #" + i; // this is TEMPORARY until we finish the player names scene/code
-//			Player newPlayer = new Player(playerName); // TODO get custom player name
-//			match.addPlayer(newPlayer);
-//		}
-//		
-//	}
-	
 	
 }
